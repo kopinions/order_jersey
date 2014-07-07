c4b6c48afd5a344e6bf88e93a07b42f49528a03f
jksun <jksun@thoughtworks.com>
2014-07-07 22:30:29 +0800

    create product price with give price

--- src/main/java/thoughtworks/com/PricesResource.java
+++ src/main/java/thoughtworks/com/PricesResource.java
@@ -9,6 +9,10 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
 
 public class PricesResource {
 
@@ -30,8 +34,17 @@ public class PricesResource {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response createProductPrice(@Context UriInfo uriInfo) {
-        int priceId = productRepository.createProductPrice(product, new Price());
+    public Response createProductPrice(@Context UriInfo uriInfo, Map price) {
+        double amount = Double.valueOf(price.get("amount").toString());
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date effectDate;
+
+        try {
+            effectDate = format.parse(price.get("effectDate").toString());
+        } catch (ParseException e) {
+            return Response.status(400).build();
+        }
+        int priceId = productRepository.createProductPrice(product, new Price(amount, effectDate));
         return Response.created(uriInfo.getAbsolutePathBuilder().path(String.valueOf(priceId)).build()).build();
     }
 }
--- src/test/thoughtworks/com/PriceResourceTest.java
+++ src/test/thoughtworks/com/PriceResourceTest.java
@@ -5,6 +5,8 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import thoughtworks.com.domain.Price;
@@ -17,6 +19,8 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -24,12 +28,20 @@ import static org.hamcrest.core.StringEndsWith.endsWith;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PriceResourceTest extends JerseyTest {
     @Mock
     ProductRepository productRepository;
+
+
+    @Captor
+    ArgumentCaptor<Product> productArgumentCaptor;
+
+    @Captor
+    ArgumentCaptor<Price> priceArgumentCaptor;
 
     @Override
     protected Application configure() {
@@ -68,13 +80,21 @@ public class PriceResourceTest extends JerseyTest {
     }
 
     @Test
-    public void should_create_product_price() {
+    public void should_create_product_price() throws ParseException {
         when(productRepository.createProductPrice(any(Product.class), any(Price.class))).thenReturn(2);
+        when(productRepository.getProductById(eq(1))).thenReturn(new Product("name", "description"));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         HashMap<Object, Object> price = new HashMap<>();
         price.put("amount", 1);
         price.put("effectDate", "2014-01-01");
         Response response = target("/products/1/prices").request().post(Entity.entity(price, MediaType.APPLICATION_JSON_TYPE));
         assertThat(response.getStatus(), is(201));
         assertThat(response.getLocation().toString(), endsWith("/products/1/prices/2"));
+
+        verify(productRepository).createProductPrice(productArgumentCaptor.capture(), priceArgumentCaptor.capture());
+        assertThat(productArgumentCaptor.getValue().getName(), is("name"));
+
+        assertThat(priceArgumentCaptor.getValue().getAmount(), is(1.0));
+        assertThat(priceArgumentCaptor.getValue().getEffectDate(), is(dateFormat.parse("2014-01-01")));
     }
 }
